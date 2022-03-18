54ff6c7d8a444455b233cabcd71892d3c0a99406
dikamuharman <dika1254@gmail.com>
2022-03-18 14:37:49 +0700

    init project

---
+++ api/generate.js
@@ -0,0 +1,15 @@
+import {createMimeMessage} from 'mimetext'
+
+export default function handler(request, response) {
+  const { sender, to, subject, message, filename, byteName } = request.query;
+  const [xlsx, zip] = filename;
+  const [xlsxByte, zipByte] = byteName;
+  const msg = createMimeMessage();
+  msg.setSender(`from ${sender}`);
+  msg.setTo(to);
+  msg.setSubject(subject);
+  msg.setMessage( 'text/plain',message);
+  msg.setAttachment(xlsx,'text/plain',msg.toBase64(xlsxByte));
+  msg.setAttachment(zip,'text/plain',msg.toBase64(zipByte));
+  response.status(200).json({'raw' : msg.asRaw()});
+}
